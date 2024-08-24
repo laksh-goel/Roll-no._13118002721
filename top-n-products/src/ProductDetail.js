bb3337cc2abd085581fcbb4ad9571816caf80c49
laksh-goel <goellakshya09@gmail.com>
2024-08-24 14:58:55 +0530

    making main components

---
+++ top-n-products/src/ProductDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function ProductDetail() {
+  return (
+    <div>
+      <h1>Product Detail page</h1>
+    </div>
+  );
+}
+
+export default ProductDetail;
---
+++ top-n-products/src/ProductList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function ProductList() {
+  return (
+    <div>
+      <h1>Product List Page</h1>
+    </div>
+  );
+}
+
+export default ProductList;
